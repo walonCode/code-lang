1c6103ac9be120ab4302e60a37e7786e499eb726
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-17 20:33:24 +0000

    add test for the ast

---
+++ ast/ast_test.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/walonCode/code-lang/token"
+)
+
+func TestString(t *testing.T){
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{ Type: token.LET, Literal: "let"},
+				Name: &Indentifier{
+					Token: token.Token{ Type: token.IDENT, Literal:"myVar"},
+					Value: "myVar",
+				},
+				Value: &Indentifier{
+					Token: token.Token{Type: token.IDENT, Literal:"anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	
+	if program.String() != "let myVar = anotherVar;"{
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
