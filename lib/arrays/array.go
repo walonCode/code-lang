d05acfcd1bd3b5ce1f5519b2c6a1d83ab8dca9aa
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-18 23:05:21 +0000

    feat: Implement new built-in functions for general utilities (`print`, `len`) and array manipulation (`first`, `last`, `rest`, `push`).

---
+++ lib/arrays/array.go
@@ -0,0 +1,85 @@
+package arrays
+
+import "github.com/walonCode/code-lang/object"
+
+var ArrayBuiltins = map[string]*object.Builtin{
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return object.NewError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return object.NewError("argument to `first` must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+			return object.NULL
+		},
+	},
+	"last": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return object.NewError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return object.NewError("argument to `last` must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[length-1]
+			}
+			return object.NULL
+		},
+	},
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return object.NewError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return object.NewError("argument to `rest` must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElement := make([]object.Object, length-1)
+				copy(newElement, arr.Elements[1:length])
+				return &object.Array{Elements: newElement}
+			}
+			return object.NULL
+		},
+	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return object.NewError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return object.NewError("argument to `rest` must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			newElement := make([]object.Object, length+1)
+			copy(newElement, arr.Elements)
+			newElement[length] = args[1]
+
+			return &object.Array{Elements: newElement}
+		},
+	},
+}
---
+++ lib/general/general.go
@@ -0,0 +1,34 @@
+package general
+
+import (
+	"fmt"
+
+	"github.com/walonCode/code-lang/object"
+)
+
+var GeneralBuiltins = map[string]*object.Builtin{
+	"print": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, value := range args {
+				fmt.Println(value.Inspect())
+			}
+			return object.NULL
+		},
+	},
+	"len": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return object.NewError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return object.NewError("argument to `len` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+}
