bc31ccf8b94d6d9a84269985a66f3a8b80ecd54e
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-18 00:26:20 +0000

    update the repl to add the parser

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/walonCode/code-lang/lexer"
-	"github.com/walonCode/code-lang/token"
+	"github.com/walonCode/code-lang/parser"
 )
 
 const PROMPT = ">> "
@@ -25,9 +25,21 @@ func Start(in io.Reader, out io.Writer){
 		line := scanner.Text()
 		
 		l := lexer.New(line)
+		p := parser.New(l)
 		
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		programe := p.ParsePrograme()
+		if len(p.Errors()) != 0 {
+			printParserError(out, p.Errors())
+			continue
 		}
+		
+		io.WriteString(out, programe.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserError(out io.Writer, errors []string){
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
