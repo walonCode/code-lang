161a7455663ce34fab4d6487983aec27d98fcbb8
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-18 21:15:41 +0000

    add the ability to run a file or open the repl

--- main.go
+++ main.go
@@ -4,11 +4,40 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"path/filepath"
 
 	"github.com/walonCode/code-lang/repl"
 )
 
 func main() {
+	args := os.Args[1:]
+	
+	if len(args) > 0 {
+		ruleFile(args[0])
+		return
+	}
+	
+	//default case if no file provided
+	runRepl()
+}
+
+func ruleFile(path string){
+	if filepath.Ext(path) != ".cl" {
+		fmt.Fprintf(os.Stderr, "Error: File %s must have a .cl extension\n", path)
+		os.Exit(1)
+	}
+	
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: could not open file %s\n",path)
+		os.Exit(1)
+	}
+	defer file.Close()
+	
+	repl.Start(file, os.Stdout)
+}
+
+func runRepl(){
 	usr, err := user.Current()
 	if err != nil {
 		panic(err)
