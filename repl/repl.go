cfd705247b975b21defcfea2e29723fbbb789bf6
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-18 00:35:05 +0000

    add asci art for code lang

--- repl/repl.go
+++ repl/repl.go
@@ -38,7 +38,20 @@ func Start(in io.Reader, out io.Writer){
 	}
 }
 
+const CODE_LANG = `
+______          __                 __                         
+/ ____/  ____   / /  ___           / /   ____ _   ____    ____ _
+/ /      / __ \ / /  / _ \  ______ / /   / __ /  / __ \  / __ /
+/ /___   / /_/ // /  /  __/ /_____// /___/ /_/ /  / / / / / /_/ / 
+\____/   \____//_/   \___/        /_____/\__,_/  /_/ /_/  \__, /  
+                                                      /____/   
+
+`                                                    
+
 func printParserError(out io.Writer, errors []string){
+	io.WriteString(out, CODE_LANG)
+	io.WriteString(out, "Whoops! We can in to some Code_lang business!\n")
+	io.WriteString(out," parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
