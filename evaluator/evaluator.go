2e9014d036535469155e0f963f9183eff605e254
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-18 02:07:52 +0000

    add the eval for boolean and int

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/walonCode/code-lang/object"
 )
 
+var (
+	NULL = &object.Null{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node)object.Object{
 	switch node := node.(type){
 		//statement
@@ -16,6 +22,8 @@ func Eval(node ast.Node)object.Object{
 		//expression
 		case *ast.IntegerLiteral:
 			return &object.Integer{ Value: node.Value}
+		case *ast.Boolean:
+			return nativeBoolToBooleanObject(node.Value)
 	}
 	
 	return nil
@@ -29,4 +37,11 @@ func evalStatements(stmt []ast.Statement)object.Object{
 	}
 	
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool)*object.Boolean{
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
