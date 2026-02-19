a18a23590def97866b0eea1f1e0a5ea9e6afcdf0
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-19 09:27:11 +0000

    add the hash and geting a value from a hash to the evaluator and object and add test for them

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,7 +5,6 @@ import (
 	"github.com/walonCode/code-lang/object"
 )
 
-
 func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
 	//statement
@@ -89,18 +88,64 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return index
 		}
 		return evalIndexExpression(left, index)
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
 	}
 
 	return nil
+}
+
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+
+	for keyNode, valueNode := range node.Pairs {
+		key := Eval(keyNode, env)
+		if isError(key) {
+			return key
+		}
+
+		hashkey, ok := key.(object.Hashable)
+		if !ok {
+			return object.NewError("unusable as hash key: %s", key.Type())
+		}
+
+		value := Eval(valueNode, env)
+		if isError(value) {
+			return value
+		}
+
+		hashed := hashkey.HashKey()
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+	}
+
+	return &object.Hash{Pairs: pairs}
 }
 
 func evalIndexExpression(left, index object.Object) object.Object {
 	switch {
 	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
 		return evalArrayIndexExpression(left, index)
+	case left.Type() == object.HASH_OBJ:
+		return evalHashIndexExpression(left, index)
 	default:
 		return object.NewError("index operator not supported: %s", left.Type())
 	}
+}
+
+func evalHashIndexExpression(hash, index object.Object)object.Object{
+	hashObj := hash.(*object.Hash)
+	
+	key, ok := index.(object.Hashable)
+	if !ok{
+		return object.NewError("unusable as hash key: %s", index.Type())
+	}
+	
+	pair, ok := hashObj.Pairs[key.HashKey()]
+	if !ok{
+		return object.NULL
+	}
+	
+	return pair.Value
 }
 
 func evalArrayIndexExpression(left, index object.Object) object.Object {
