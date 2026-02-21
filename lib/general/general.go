1abe3044c9c73c46b0bc128dcf380bf8e6ee73ab
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-21 12:19:41 +0000

    add the typeof and improve the print

--- lib/general/general.go
+++ lib/general/general.go
@@ -7,11 +7,49 @@ import (
 	"github.com/walonCode/code-lang/object"
 )
 
+func unwrapObject(obj object.Object)any {
+	switch o := obj.(type){
+	case *object.Integer:
+		return o.Value
+	case *object.String:
+		return o.Value
+	case *object.Boolean:
+		return o.Value
+	case *object.Float:
+		return o.Value	
+	case *object.Char:
+		return o.Value
+	default:
+		return o.Inspect()
+	}
+}
+
 var GeneralBuiltins = map[string]*object.Builtin{
 	"print": {
 		Fn: func(node *ast.CallExpression, args ...object.Object) object.Object {
-			for _, value := range args {
-				fmt.Println(value.Inspect())
+
+			if len(args) == 0 {
+				fmt.Println()
+				return nil
+			}
+
+			formatStr, isFormat := args[0].(*object.String)
+
+			if isFormat && len(args) > 1 {
+				goArgs := make([]any, len(args) -1 )
+				for i, arg := range args[1:] {
+					goArgs[i] = unwrapObject(arg)
+				}
+				fmt.Printf(formatStr.Value, goArgs...)
+				fmt.Println()
+			}else {
+				for i, value := range args {
+					fmt.Print(value.Inspect())
+					if i < len(args) - 1 {
+						fmt.Print(" ")
+					}
+				}
+				fmt.Println()
 			}
 			return nil
 		},
@@ -30,6 +68,14 @@ var GeneralBuiltins = map[string]*object.Builtin{
 			default:
 				return object.NewError(node.Line(), node.Column(),"argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"typeof": {
+		Fn: func(node *ast.CallExpression, args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return object.NewError(node.Line(), node.Column(),"wrong number of arguments. got=%d, want=1", len(args))
+			}
+			return &object.String{Value: string(args[0].Type())}
 		},
 	},
 }
