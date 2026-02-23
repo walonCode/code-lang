264b49c199e550f84bb9a352ff24236d7147f1dd
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-23 15:38:17 +0000

    feat: Add `net` standard library module and preload `http`, `json`, and `net` modules.

--- evaluator/builtin.go
+++ evaluator/builtin.go
@@ -3,6 +3,8 @@ package evaluator
 import (
 	"github.com/walonCode/code-lang/std/arrays"
 	"github.com/walonCode/code-lang/std/general"
+	"github.com/walonCode/code-lang/std/json"
+	"github.com/walonCode/code-lang/std/net"
 )
 
 
@@ -10,4 +12,7 @@ func init() {
     // preload standard library modules
     moduleCache["arrays"]  = arrays.Module()
     moduleCache["fmt"] = general.Module()
+    moduleCache["http"] = net.HttpModule()
+    moduleCache["json"] = json.JsonModule()
+    moduleCache["net"] = net.NetModule()
 }
